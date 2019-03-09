5adccfe3e637724f27bb26d3f4306b54d122ff16
Voswinkel <likofos@gmail.com>
2019-03-09 18:01:03 +0100

    1 и 2 готовы 3 - цикл 71 строки подвешивает программу

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -11,63 +11,69 @@ function calculateQuadraticEquation() {
 }
 
 function getResult(a, b, c) {
-  let d = b * b - 4 * a * c, result;
+  let d = b * b - 4 * a * c,
+    result;
 
   if (d < 0) {
-    return("уравнение не имеет корней");
+    return ("уравнение не имеет корней");
+  } else if (a === 0) {
+    result = (-c) / b;
+    return `При а = 0 уравнение решается как линейное x= ` + result;
   } else if (d === 0) {
-    result = (b + (Math.sqrt(d)) / (2 * a));
+    result === 0;
     return result;
-    } else if (d > 0) {
-        var x1 = (-b + (Math.sqrt(d)) / (2 * a)),
-            x2 = (b + (Math.sqrt(d)) / (2 * a));
-            result = [x1, x2];
-        return result;
-      }
+  } else if (d > 0) {
+    var x1 = (-b + (Math.sqrt(d)) / (2 * a)),
+      x2 = (b + (Math.sqrt(d)) / (2 * a));
+    result = [x1, x2];
+    return result;
+  }
 }
 
 
 
 
-      function calculateDrinkTask() {
-        let name = window.personName.value;
-        let dateOfBirthday = new Date(window.dateOfBirthday.value);
-        let drink = askDrink(name, dateOfBirthday);
-        window.drink.textContent = drink;
-      }
+function calculateDrinkTask() {
+  let name = window.personName.value;
+  let dateOfBirthday = new Date(window.dateOfBirthday.value);
+  let drink = askDrink(name, dateOfBirthday);
+  window.drink.textContent = drink;
+}
 
-      function askDrink(name, dateOfBirthday) {
-        let yearOfBirth = dateOfBirthday.getFullYear(),
-            presentYear = new Date().getFullYear(),
-            age = (presentYear - yearOfBirth);
+function askDrink(name, dateOfBirthday) {
+  let yearOfBirth = dateOfBirthday.getFullYear(),
+    presentYear = new Date().getFullYear(),
+    age = (presentYear - yearOfBirth);
 
-        if (age < 18) {
-        return age;
-        alert(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
-        } else {
-          return age;
-          alert(`Не желаете ли олд-фэшн, ${name}?`)
-        }
-        console.log(age);
-      }
+  if (age < 18) {
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот! ` + age;
+  } else {
+    return `Не желаете ли олд-фэшн, ${name}?  ` + age;
+  }
+  console.log(age);
+}
 
-      function calculateAverageRating() {
-        let marks = window.marks.value.split("").map(Number);
-        let averageMark = getAverageMark(marks);
-        window.averageMark.textContent = averageMark;
-      }
+function calculateAverageRating() {
+  let marks = window.marks.value.split("").map(Number);
+  let averageMark = getAverageMark(marks);
+  window.averageMark.textContent = averageMark;
+}
 
-      function getAverageMark(marks) {
-       // код для задачи №3 писать здесь
-        let totalAmountOfMarks, mark, avaerageNote;
-        for ( let i = 0; marks.length > 5; marks.splice(5)) {
-           console.log("Количество оценок большe пяти");
-           console.log(marks);
-          }
-        for ( mark in marks) {
-        console.log(marks);
-        }
+function getAverageMark(marks) {
+  let totalAmountOfMarks = 0,
+    avaerageNote;
 
-        avaerageNote = totalAmountOfMarks / marks.length;
-        return avaerageNote;
-    }
+  if (marks.length > 5) {
+    marks.splice(5);
+    console.log("Количество оценок большe пяти");
+  }
+
+  for (let i = 0; marks.length < 5; i++) {
+    totalAmountOfMarks = marks[i] + totalAmountOfMarks;
+    avaerageNote = totalAmountOfMarks / marks.length;
+    console.log(totalAmountOfMarks);
+  }
+
+
+  return `Средняя оценка: ` + avaerageNote;
+}
