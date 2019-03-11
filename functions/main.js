019dde1279cf5d8b38354a3509e10dbf3e5f5e14
Voswinkel <likofos@gmail.com>
2019-03-11 13:13:19 +0100

    в глобальный объект не записываются данные из функции

--- functions/main.js
+++ functions/main.js
@@ -1,32 +1,62 @@
 // Task 1
-let a = +prompt("Input a", ""),
-  b = +prompt("Input b", ""),
-  c = +prompt("Input c", ""),
-  x1,
-  x2,
-  d = b * b - 4 * a * c;
+// let a = +prompt("Input a", ""),
+//   b = +prompt("Input b", ""),
+//   c = +prompt("Input c", ""),
+//   diskriminant,
+//   x1,
+//   x2,
+//   roots = [x1, x2],
+//   d = b * b - 4 * a * c;
+// result = {
+//   diskriminant,
+//   roots
+// }
+// result.diskriminant === d;
+//
+// getSolutions(a, b, c);
+// showSolutionsMessage(a, b, c);
+//
+// function getSolutions(a, b, c) {
+//   if (d < 0) {
+//     return result = "Дискриминант меньше нуля, уравнение не имеет корней";
+//   } else if (d === 0) {
+//     x1 = -b / 2 * a;
+//     return result;
+//   } else if (d > 0) {
+//     diskriminant === d,
+//       x1 = (-b + (Math.sqrt(d)) / (2 * a)),
+//       x2 = (b + (Math.sqrt(d)) / (2 * a));
+//     return result;
+//   }
+// }
+//
+// function showSolutionsMessage(a, b, c) {
+//   result = getSolutions(a, b, c);
+//   console.log(result);
+// }
 
-getSolutions(a, b, c);
-
-function getSolutions(a, b, c) {
-  let result = {
-    d,
-    x1,
-    x2
-  }
-  result.d === d;
-
-  if (d < 0) {
-    alert ("D:" + result.d);
-  } else if (a === 0) {
-    result.x1 = (-c) / b;
-    alert("При а = 0 уравнение решается как линейное x= " + result.x1);
-  } else if (d === 0) {
-    result.x1 = -b / 2 * a;
-    alert("значение_x1: " + result.x1 + "  D: " + result.d);
-  } else if (d > 0) {
-    result.x1 = (-b + (Math.sqrt(d)) / (2 * a)),
-      result.x2 = (b + (Math.sqrt(d)) / (2 * a));
-    alert("D: " + result.d + "значение_x1: " + result.x1 + "  значение_x2: " +result.x2);
-  }
+//Task 2
+let num = 1, num1 = 0,
+secretData = {
+  aaa: num,
+  bbb: num1
 }
+
+function personalData(secretData) {
+  let aaa = "firstname",
+    bbb = "lastName";
+  if (num === 0) {
+    num = "Родриго";
+  } else {
+    num = "Эмилио";
+  }
+
+  if (num1 === 0) {
+    num1 = "Родриго";
+  } else {
+    num1 = "Эмилио";
+  }
+console.log(secretData);
+}
+
+personalData(secretData);
