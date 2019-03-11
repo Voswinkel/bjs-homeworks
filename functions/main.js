2e9d9ecfba995631f42fddaa7d248b087cf11abb
Voswinkel <likofos@gmail.com>
2019-03-11 22:57:06 +0100

    task1

--- functions/main.js
+++ functions/main.js
@@ -1,62 +1,59 @@
 // Task 1
-// let a = +prompt("Input a", ""),
-//   b = +prompt("Input b", ""),
-//   c = +prompt("Input c", ""),
-//   diskriminant,
-//   x1,
-//   x2,
-//   roots = [x1, x2],
-//   d = b * b - 4 * a * c;
-// result = {
-//   diskriminant,
-//   roots
-// }
-// result.diskriminant === d;
-//
-// getSolutions(a, b, c);
-// showSolutionsMessage(a, b, c);
-//
-// function getSolutions(a, b, c) {
-//   if (d < 0) {
-//     return result = "Дискриминант меньше нуля, уравнение не имеет корней";
-//   } else if (d === 0) {
-//     x1 = -b / 2 * a;
-//     return result;
-//   } else if (d > 0) {
-//     diskriminant === d,
-//       x1 = (-b + (Math.sqrt(d)) / (2 * a)),
-//       x2 = (b + (Math.sqrt(d)) / (2 * a));
-//     return result;
-//   }
-// }
-//
-// function showSolutionsMessage(a, b, c) {
-//   result = getSolutions(a, b, c);
-//   console.log(result);
-// }
+let a = +prompt("Input a", ""),
+  b = +prompt("Input b", ""),
+  c = +prompt("Input c", ""),
+  result = new Object;
+
+getSolutions(a, b, c);
+//sshowSolutionsMessage(a, b, c);
+
+function getSolutions(a, b, c) {
+  let x1, roots,
+    d = b * b - 4 * a * c;
+  result.d === d;
+
+  if (d < 0) {
+    console.log(result.d);
+  } else if (d === 0) {
+    x1 = -b / 2 * a;
+    result.roots = [x1];
+console.log(result.roots);
+  } else if (d > 0) {
+    let x2 = (b + (Math.sqrt(d)) / (2 * a)),
+    x1 = (-b + (Math.sqrt(d)) / (2 * a));
+    result.roots = [x1, x2];
+    console.log(result.roots, result.d);
+  }
+}
+
+function showSolutionsMessage(a, b, c) {
+  solution = getSolutions(a, b, c);
+  console.log(getSolutions(a, b, c));
+}
 
 //Task 2
-let num = 1, num1 = 0,
-secretData = {
-  aaa: num,
-  bbb: num1
-}
-
-function personalData(secretData) {
-  let aaa = "firstname",
-    bbb = "lastName";
-  if (num === 0) {
-    num = "Родриго";
-  } else {
-    num = "Эмилио";
+let num = 1,
+  num1 = 0,
+  secretData = {
+    aaa: num,
+    bbb: num1
   }
-
-  if (num1 === 0) {
-    num1 = "Родриго";
-  } else {
-    num1 = "Эмилио";
-  }
-console.log(secretData);
-}
+// 
+// function personalData(secretData) {
+//   let aaa = "firstname",
+//     bbb = "lastName";
+//   if (num === 0) {
+//     num = "Родриго";
+//   } else {
+//     num = "Эмилио";
+//   }
+//
+//   if (num1 === 0) {
+//     num1 = "Родриго";
+//   } else {
+//     num1 = "Эмилио";
+//   }
+//   console.log(secretData);
+// }
 
 personalData(secretData);
