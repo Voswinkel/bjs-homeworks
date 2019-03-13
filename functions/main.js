f78f084cc580fe693c85a099353b64e4ae5baa79
Voswinkel <likofos@gmail.com>
2019-03-13 21:58:38 +0100

    1 упр

--- functions/main.js
+++ functions/main.js
@@ -1,35 +1,34 @@
 // Task 1
-let a = +prompt("Input a", ""),
-  b = +prompt("Input b", ""),
-  c = +prompt("Input c", ""),
-  result = new Object,
-  d = b * b - 4 * a * c;
+let a = 2,
+  b = 8,
+  c = 2;
 
 getSolutions(a, b, c);
-//sshowSolutionsMessage(a, b, c);
+showSolutionsMessage(a, b, c);
 
 function getSolutions(a, b, c) {
-  let x1, roots,
-    d = b * b - 4 * a * c;
-  result.d === d;
-
+     let d = b * b - 4 * a * c;
+let result = new Object();
+result.diskriminant = d;
+result.roots;
   if (d < 0) {
-    console.log(result.d);
+    return result.diskriminant;
   } else if (d === 0) {
     x1 = -b / 2 * a;
     result.roots = [x1];
-console.log(result.roots);
+return result.roots, result.diskriminant;
   } else if (d > 0) {
     let x2 = (b + (Math.sqrt(d)) / (2 * a)),
     x1 = (-b + (Math.sqrt(d)) / (2 * a));
     result.roots = [x1, x2];
-    console.log(result.roots, result.d);
+    return result.roots, result.diskriminant;
   }
 }
 
 function showSolutionsMessage(a, b, c) {
-  solution = getSolutions(a, b, c);
-  console.log(getSolutions(a, b, c));
+  let result = getSolutions(a, b, c);
+console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+return result;
 }
 
 //Task 2
