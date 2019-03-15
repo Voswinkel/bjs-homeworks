c5d492c47f7283e95c533e6db661ca98e8c4747c
Voswinkel <likofos@gmail.com>
2019-03-15 19:42:27 +0100

    Task 1

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,39 +1,52 @@
 function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
+  const birthday = document.getElementById('birthday').value;
 
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
+  const result = checkBirthday(birthday) ? "Да" : "Нет";
 
-    document.getElementById('disclaimer').innerHTML = result;   
+  document.getElementById('disclaimer').innerHTML = result;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
+  // код для задачи №1 писать здесь
+  let now = new Date();
+  now = Date.parse(now);
+  birthday = Date.parse(birthday);
+
+  let diff = now - birthday;
+  age = diff / 31536000000;
+  age = Math.round(age);
+  if (age < 18) {
+    console.log(`Несовершеннолетний: ${age} лет`)
+  } else {
+    console.log(`Совершеннолетний: ${age} лет`)
+  }
+  return age;
 }
 
 function initPrintAnimalSound() {
-    const animal = {
-        sound: 'grrrr',
-    };
+  const animal = {
+    sound: 'grrrr',
+  };
 
-    const result = getAnimalSound(animal);
+  const result = getAnimalSound(animal);
 
-    document.getElementById('sound').innerHTML = result;   
+  document.getElementById('sound').innerHTML = result;
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+  // код для задачи №2 писать здесь
 }
 
 function initCalculateStatement() {
-    for (let idx = 0; idx < 3; idx++) {
-        const marks = document.getElementById('learner-' + idx).value.split(',');
+  for (let idx = 0; idx < 3; idx++) {
+    const marks = document.getElementById('learner-' + idx).value.split(',');
 
-        const average = getAverageMark(marks);
+    const average = getAverageMark(marks);
 
-        document.getElementById('learner-' + idx + '-average').innerHTML = average;
-    }
+    document.getElementById('learner-' + idx + '-average').innerHTML = average;
+  }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+  // код для задачи №3 писать здесь
 }
